5247dd02700b240b1a2f2895f943892d2a381f1f
Doc1998 <docmp98@gmail.com>
2024-05-17 17:16:16 +0000

    updates

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -11,17 +11,7 @@
         ariaExpanded();
     });
 
-    // checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
-    function ariaExpanded() {
-        const csUL = document.querySelector('#cs-expanded');
-        const csExpanded = csUL.getAttribute('aria-expanded');
 
-        if (csExpanded === 'false') {
-            csUL.setAttribute('aria-expanded', 'true');
-        } else {
-            csUL.setAttribute('aria-expanded', 'false');
-        }
-    }
 
     // mobile nav dropdown code
     const dropDowns = Array.from(document.querySelectorAll('#cs-navigation-760 .cs-dropdown'));
