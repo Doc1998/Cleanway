cef0c4af9b04a244feacc6db99cb9de37180d071
Doc1998 <docmp98@gmail.com>
2024-08-30 12:43:23 +0100

    updates

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -21,4 +21,15 @@
         }
         item.addEventListener('click', onClick)
         }
+            
+        
+        const toggle = document.querySelector("#pricing-603 .cs-toggle");
+        const cardGroup = Array.from(document.querySelectorAll('.cs-card-group'))
+
+        toggle.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.toggle("active");
+            }
+            toggle.classList.toggle("active");
+        });
                                 
